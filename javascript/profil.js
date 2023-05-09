c0441a64e08e37d1ba54f9d2e1389ed2af4fdff8
rayhearthdars <104355268+rayhearthdars@users.noreply.github.com>
2023-05-09 09:25:19 +0200

    réorganisation du css

--- javascript/profil.js
+++ javascript/profil.js
@@ -1,25 +1,27 @@
 // /*Images gallery */
 
-var images = [  //create new array with images to show in gallery
+var images = [
+    //create new array with images to show in gallery
     "image/jeu_de_societe.jpg",
     "image/randonnee.jpg",
     "image/lecture.jpg",
     "image/video_game.jpg",
-    "image/peinture.jpg"
+    "image/peinture.jpg",
 ];
 
-var figcaptions = [  //create array with captions to show in gallery
+var figcaptions = [
+    //create array with captions to show in gallery
     "Jeux de société",
     "Randonnée",
     "Lecture",
     "Jeux Vidéo",
-    "Peinture"
-]
+    "Peinture",
+];
 
 var num = 0;
 function next() {
-    let slider = document.getElementById("slider"); // get first image
-    let caption_slider = document.getElementById("figcaption"); // get first caption
+    let slider = document.getElementsByClassName("slider").item(0); // get first image
+    let caption_slider = document.getElementsByClassName("figcaption").item(0); // get first caption
     num++;
     if (num >= images.length) {
         num = 0;
@@ -27,161 +29,166 @@ function next() {
     slider.src = images[num]; // source of el slider changes according to num
     caption_slider.textContent = figcaptions[num]; // same for text content of caption
 }
-function previous() { // same in reverse
-    let slider = document.getElementById("slider");
-    let caption_slider = document.getElementById("figcaption");
+function previous() {
+    // same in reverse
+    let slider = document.getElementsByClassName("slider").item(0);
+    let caption_slider = document.getElementsByClassName("figcaption").item(0);
     num--;
     if (num < 0) {
-        num = images.length -1;
+        num = images.length - 1;
     }
     slider.src = images[num];
     caption_slider.textContent = figcaptions[num];
 }
 
-let prevImage = document.getElementById("previous");
-let nextImage = document.getElementById("next");
+let prevImage = document.getElementsByClassName("previous").item(0);
+let nextImage = document.getElementsByClassName("next").item(0);
 
-prevImage.addEventListener("click", function() {previous();}, false); // add event on click to id = previous button
-nextImage.addEventListener("click", function() {next();}, false); // add event on click to id = next button
+prevImage.addEventListener(
+    "click",
+    function () {
+        previous();
+    },
+    false
+); // add event on click to id = previous button
+nextImage.addEventListener(
+    "click",
+    function () {
+        next();
+    },
+    false
+); // add event on click to id = next button
 /**************************************************************** */
 
 // Create blocktype element with text content add it as parent's child.
-// If fill is true, create multiple element. If false create only one. 
+// If fill is true, create multiple element. If false create only one.
 // If name is given, add class to element.
 // Return handle to it.
-let soft_skills__et_langues = document.getElementsByClassName('soft_skills__et_langues')[0];
+let softSkillsAndLanguages = document.getElementsByClassName("container_language_soft_skills").item(0);
 
-
-function addElement (text, blockType, parent, fill, numTop, name){
-    let el= document.createElement(blockType);
+function addElement(text, blockType, parent, fill, numTop, name) {
+    let el = document.createElement(blockType);
     let newContent = document.createTextNode(text);
-    el.appendChild(newContent);        
+    el.appendChild(newContent);
     parent.appendChild(el);
 
     let parentWidth = window.innerWidth;
     let width = el.offsetWidth;
     let height = el.offsetHeight;
-    let multiplier = (parentWidth/width) / 2 ; // calculates the number of element to add to html so that it covers the entire screen
-    let paddingSides = (((parentWidth) - (width * multiplier)) / (multiplier)/2);  //calculates padding to add to each element
+    let multiplier = parentWidth / width / 2; // calculates the number of element to add to html so that it covers the entire screen
+    let paddingSides = (parentWidth - width * multiplier) / multiplier / 2; //calculates padding to add to each element
 
-    if (numTop !== -1 ) {
-        el.style.top = height*(numTop) + 15 + 'px'; //position each el[0] from top according to their number (in order of their apparition)
-   }
+    if (numTop !== -1) {
+        el.style.top = height * numTop + 15 + "px"; //position each el[0] from top according to their number (in order of their apparition)
+    }
 
-    if (name != undefined){
+    if (name != undefined) {
         el.classList.add(name);
     }
-    
-    if (fill) { //for the first element of the elements that are repeated
-        el.style.position = 'absolute';
-        el.style.paddingLeft = paddingSides + 'px';
-        el.style.paddingRight = paddingSides + 'px';           
+
+    if (fill) {
+        //for the first element of the elements that are repeated
+        el.style.position = "absolute";
+        el.style.paddingLeft = paddingSides + "px";
+        el.style.paddingRight = paddingSides + "px";
     }
 
-    if (fill) { //for all the elements that are repeated except the first
-        for (let i = 0; i < multiplier -1; i++) {
-            let el= document.createElement(blockType);
+    if (fill) {
+        //for all the elements that are repeated except the first
+        for (let i = 0; i < multiplier - 2; i++) {
+            let el = document.createElement(blockType);
             let newContent = document.createTextNode(text);
             el.appendChild(newContent);
             parent.appendChild(el);
-            el.style.paddingLeft = paddingSides + 'px';
-            el.style.paddingRight = paddingSides + 'px'; 
+            el.style.paddingLeft = paddingSides + "px";
+            el.style.paddingRight = paddingSides + "px";
             el.classList.add(name);
-            el.style.position = 'absolute';              
-            
-            if (numTop !== -1 ) {
-                el.style.top = height*(numTop) + 15 + 'px';  //position each el[i] from top according to their number (in order of their apparition) (except first one)
-           }        
+            el.style.position = "absolute";
+
+            if (numTop !== -1) {
+                el.style.top = height * numTop + 15 + "px"; //position each el[i] from top according to their number (in order of their apparition) (except first one)
+            }
         }
     }
-    
-let heightOfSoft_skills__et_langues = height * 7; 
-soft_skills__et_langues.style.height = heightOfSoft_skills__et_langues + 'px';
-       
+
+    let heightOfsoftSkillsAndLanguages = height * 9;
+    softSkillsAndLanguages.style.height = heightOfsoftSkillsAndLanguages + "px";
+
     return el;
 }
 
-
 //Create new el div for soft skills and langues and h2 then ul for each el then several li for each ul  and then multiple p for each li
-let div = addElement('', 'div', soft_skills__et_langues, false, -1);
-addElement('Soft skills', 'h2', div, true, 0, 'skill');
-let ulEl = addElement('', 'ul', soft_skills__et_langues, false, -1);
+let div = addElement("", "div", softSkillsAndLanguages, false, -1);
+addElement("Soft skills", "h2", div, true, 0, "skill");
+let ulEl = addElement("", "ul", softSkillsAndLanguages, false, -1);
 // let liEl = addElement('', 'li', ulEl, false, -1)
 // addElement('Apprentissage rapide', 'p', liEl, true, 1, 'app');
-liEl = addElement('', 'li', ulEl, false, -1);
-addElement('Curiosité', 'p', liEl, true, 1, 'curi');
-liEl = addElement('', 'li', ulEl, false, -1);
-addElement('Rigueur', 'p', liEl, true, 2, 'rig');
-liEl = addElement('', 'li', ulEl, false, -1);
-addElement("Organisée", 'p', liEl, true, 3, 'org');
-div = addElement('', 'div', soft_skills__et_langues, false, -1);
-addElement('Langues', 'h2', div, true, 4, 'lang');
-ulEl = addElement('', 'ul', soft_skills__et_langues, false, -1);
-liEl = addElement('', 'li', ulEl, false, -1);
-addElement('Anglais', 'p', liEl, true, 5, 'ang');
-liEl = addElement('', 'li', ulEl, false, -1);
-addElement('Allemand', 'p', liEl, true, 6, 'all');
-
-
-
+liEl = addElement("", "li", ulEl, false, -1);
+addElement("Curiosité", "p", liEl, true, 1, "curi");
+liEl = addElement("", "li", ulEl, false, -1);
+addElement("Rigueur", "p", liEl, true, 2, "rig");
+liEl = addElement("", "li", ulEl, false, -1);
+addElement("Organisée", "p", liEl, true, 3, "org");
+div = addElement("", "div", softSkillsAndLanguages, false, -1);
+addElement("Langues", "h2", div, true, 4, "lang");
+ulEl = addElement("", "ul", softSkillsAndLanguages, false, -1);
+liEl = addElement("", "li", ulEl, false, -1);
+addElement("Anglais", "p", liEl, true, 5, "ang");
+liEl = addElement("", "li", ulEl, false, -1);
+addElement("Allemand", "p", liEl, true, 6, "all");
 
 /*let el go by without interruption*/
 
 let pos2 = window.innerWidth;
 
-function initialPositionOfElement(el){
-    for (let i=0; i < el.length; i++){
+function initialPositionOfElement(el) {
+    for (let i = 0; i < el.length; i++) {
         let position = el[0].offsetWidth * i; //length of 1 el[0] is the same for each el[i]
         el[i].style.left = position + "px";
     }
 }
 // position[0] = (elWidth * i) + elWidth;
-function moveElementsForwards (el) {
+function moveElementsForwards(el) {
     let elWidth = el[0].offsetWidth;
     for (let i = 0; i < el.length; i++) {
         let position = parseFloat(el[i].style.left.slice(0, -2)) + elWidth; //slice to remove "px" from el[i].style.left and turns string to float
-                //add elWidth to make elWidth the origin so as to make every new el[i] start at -elWidth and not 0 so we can have the impression of an uninterrupted line
-        position = (((position + 0.5) % (pos2 + elWidth))); //length of pos2 if elWidth is origin
-        el[i].style.left = (position - elWidth) + "px"; //return origin to 0
+        //add elWidth to make elWidth the origin so as to make every new el[i] start at -elWidth and not 0 so we can have the impression of an uninterrupted line
+        position = (position + 0.5) % (pos2 + elWidth); //length of pos2 if elWidth is origin
+        el[i].style.left = position - elWidth + "px"; //return origin to 0
     }
 }
 
-function moveElementsBackwards (el) {
-    function invertPos2AndOrigin(x){
-        let u = pos2 -x;  //absolute value of pos2 becomes the origin so 0 to x becomes pos2 - x
+function moveElementsBackwards(el) {
+    function invertPos2AndOrigin(x) {
+        let u = pos2 - x; //absolute value of pos2 becomes the origin so 0 to x becomes pos2 - x
         return u;
     }
-    function returnToNormal (u) {
-        let x = pos2 - u;  //0 is the origin once again so x is pos2 - u
+    function returnToNormal(u) {
+        let x = pos2 - u; //0 is the origin once again so x is pos2 - u
         return x;
     }
-    let elWidth = el[0].offsetWidth;  
+    let elWidth = el[0].offsetWidth;
     for (let i = 0; i < el.length; i++) {
-        let position = parseFloat(el[i].style.left.slice(0, -2));  //.slice to remove "px" from el[i].style.left and turns string to float
+        let position = parseFloat(el[i].style.left.slice(0, -2)); //.slice to remove "px" from el[i].style.left and turns string to float
         let posConverted = invertPos2AndOrigin(position);
-        let PositionWithPos2AsOrigin = ((posConverted + 0.5) % (pos2 + elWidth));
+        let PositionWithPos2AsOrigin = (posConverted + 0.5) % (pos2 + elWidth);
         el[i].style.left = returnToNormal(PositionWithPos2AsOrigin) + "px";
-    } 
+    }
 }
 
-
-let elClassSkill = document.getElementsByClassName('skill');
-// let elClassApp = document.getElementsByClassName('app');
-let elClassCuri = document.getElementsByClassName('curi');
-let elClassRig = document.getElementsByClassName('rig');
-let elClassOrg = document.getElementsByClassName('org');
-let elClassLang = document.getElementsByClassName('lang');
-let elClassAng = document.getElementsByClassName('ang');
-let elClassAll = document.getElementsByClassName('all');
+let elClassSkill = document.getElementsByClassName("skill");
+let elClassCuri = document.getElementsByClassName("curi");
+let elClassRig = document.getElementsByClassName("rig");
+let elClassOrg = document.getElementsByClassName("org");
+let elClassLang = document.getElementsByClassName("lang");
+let elClassAng = document.getElementsByClassName("ang");
+let elClassAll = document.getElementsByClassName("all");
 
 //call intitialPosition for every class
 // call setInterval function to call moveElementsForwards or moveElementsBackwards every 50ms
 
 let tSkilInitialPosition = initialPositionOfElement(elClassSkill);
 let tSkill = setInterval(moveElementsForwards, 50, elClassSkill);
-
-// let tAppInitialPosition = initialPositionOfElement(elClassApp);
-// let tApp = setInterval(moveElementsBackwards, 50, elClassApp);
 
 let tCuriInitialPosition = initialPositionOfElement(elClassCuri);
 let tCuri = setInterval(moveElementsBackwards, 50, elClassCuri);
